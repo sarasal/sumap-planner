2c0cc01aaeed283f2882e301642df5c257b77d87
sarasal <sara.sarnv95@gmail.com>
2024-12-16 02:41:26 +0100

    cost definition bolded

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -75,7 +75,8 @@ import QuestionModal from "@/components/QuestionModal.vue";
                   <img :src="iconsSrc.distance" alt="Icon" class="icon-small" /> <b>Driving Distance</b>: Delivering the parcels creates costs. The longer deliveries are, the more costs are created. The length of a sub-route is displayed when hovering over a particular route. <b>One minute</b> of driving equals to <b>60 cost points</b>. The speed of the car and e-bike is constant, with the car traveling at <b>70 km/h</b> and the e-bike at <b>45 km/h</b>. <br/>
                 </b-list-group-item>
                 <b-list-group-item>
-                  <img :src="iconsSrc.deliveryTime" alt="Icon" class="icon-small" /> <b>Delivery Handling Time</b>: {{this.delivery_handling_time}}
+<!--                  <img :src="iconsSrc.deliveryTime" alt="Icon" class="icon-small" /> <b>Delivery Handling Time</b>: {{this.delivery_handling_time}}-->
+                  <img :src="iconsSrc.deliveryTime" alt="Icon" class="icon-small" /> <b>Delivery Handling Time</b>: <span v-html="this.delivery_handling_time"> </span>
                 </b-list-group-item>
               </b-list-group>
               <b-card ref="ai-info" title="DeliveryPlanner AI System" style="font-size: 14px" class="delivery-card">
