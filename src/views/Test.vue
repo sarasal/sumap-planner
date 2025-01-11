196e9a592f77ee2a65c42ecc598fa0290a12e2ee
sarasal <sara.sarnv95@gmail.com>
2025-01-11 22:09:52 +0100

    task description text is updated

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -69,10 +69,10 @@ import QuestionModal from "@/components/QuestionModal.vue";
                   <img :src="iconsSrc.package" alt="Icon" class="icon-small" /> <b>Number of Parcels</b>: Each parcel delivery brings you <b>100</b> points. The routes may vary in the number of parcels (between <b>1</b> and <b>10</b>).
                 </b-list-group-item>
                 <b-list-group-item>
-                  <img :src="iconsSrc.car" alt="Icon" class="icon-small" /> / <img :src="iconsSrc.eBike" alt="Icon" class="icon-small" /> <b>Delivery Vehicle</b>: Parcels are delivered either by <b>car</b> or <b>e-bike</b>. Deliveries by e-bike are rewarded with <b>50</b> points for each traveled kilometer. Deliveries by car are faster and save time but do not come an additional reward. <br/>
+                  <img :src="iconsSrc.car" alt="Icon" class="icon-small" /> / <img :src="iconsSrc.eBike" alt="Icon" class="icon-small" /> <b>Delivery Vehicle</b>: Parcels are delivered either by <b>car</b> or <b>e-bike</b>. Deliveries by e-bike are rewarded with <b>50</b> points for each traveled kilometer. Deliveries by car are faster and save time but do not come with an additional reward. <br/>
                 </b-list-group-item>
                 <b-list-group-item>
-                  <img :src="iconsSrc.distance" alt="Icon" class="icon-small" /> <b>Driving Distance</b>: Delivering the parcels creates costs. The longer deliveries are, the more costs are created. The length of a sub-route is displayed when hovering over a particular route. <b>One minute</b> of driving equals to <b>60 cost points</b>. The speed of the car and e-bike is constant, with the car traveling at <b>70 km/h</b> and the e-bike at <b>45 km/h</b>. <br/>
+                  <img :src="iconsSrc.distance" alt="Icon" class="icon-small" /> <b>Driving Distance</b>: Delivering the parcels creates costs. The longer deliveries are, the more costs are created. The length of a sub-route is displayed when hovering over a particular route. <b>One minute</b> of driving equals <b>60 cost points</b>. The speed of the car and e-bike is constant, with the car traveling at <b>70 km/h</b> and the e-bike at <b>45 km/h</b>. <br/>
                 </b-list-group-item>
                 <b-list-group-item>
 <!--                  <img :src="iconsSrc.deliveryTime" alt="Icon" class="icon-small" /> <b>Delivery Handling Time</b>: {{this.delivery_handling_time}}-->
